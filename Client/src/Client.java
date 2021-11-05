d2793e078980256a1bdf1005dcaa63fe015386dd
Blomsgaard <70745304+Blomsgaard@users.noreply.github.com>
2021-11-05 14:51:57 +0100

    Update Client.java

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 
 public class Client implements java.io.Serializable {
 
-    static String host = "192.168.43.139";
-    static int port = 6970;
+    static String host = "192.168.43.6";
+    static int port = 6969;
 
     static DataInputStream in;
     static DataOutputStream out;
